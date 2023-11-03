71964dd4ec7f6620635c4f829f2bfe5c4d7e7de5
illustris <rharikrishnan95@gmail.com>
2023-11-04 00:28:39 +0530

    init

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+	description = "TLG's pyenv";
+
+	outputs = { nixpkgs, ... }: {
+		packages.x86_64-linux = rec {
+			default = monstrosity;
+			monstrosity = nixpkgs.legacyPackages.x86_64-linux.python3.withPackages(ps: with ps; [
+				requests mysql-connector boto3 pandas pylint backoff celery
+				sqlalchemy flask python-dotenv markdown behave
+				(
+					buildPythonPackage rec {
+						pname = "heroku3";
+						version = "5.2.1";
+						src = fetchPypi {
+							inherit pname version;
+							sha256 = "sha256-81U6FU6fWVc0vlEp7fXZHHDE4KpYI30exYTHIfrdC88=";
+						};
+						doCheck = false;
+						propagatedBuildInputs = [
+							# Specify dependencies
+							pkgs.python3Packages.dateutil
+							pkgs.python3Packages.requests
+						];
+					}
+				)
+				(
+					buildPythonPackage rec {
+						pname = "cloudflare";
+						version = "2.11.6";
+						src = fetchPypi {
+							inherit pname version;
+							sha256 = "sha256-ZTHUyvFguvME0Jl0JRzwWmJOaWPUz4RFeMEVTvupb14=";
+						};
+						doCheck = false;
+						propagatedBuildInputs = [
+							# Specify dependencies
+							pkgs.python3Packages.dateutil
+							pkgs.python3Packages.requests
+							pkgs.python3Packages.jsonlines
+							pkgs.python3Packages.pyyaml
+							pkgs.python3Packages.beautifulsoup4
+						];
+					}
+				)
+			]);
+		};
+	};
+}
